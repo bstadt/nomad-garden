c16c12d125b0f821352969e0a192837ec49ab0cb
bstadt <brandonduderstadt@gmail.com>
2023-09-09 15:56:17 -0400

    tryna fix mobile

--- components/video.tsx
+++ components/video.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function RemoteVideo(props){
     return(
     <div className="flex flex-col items-center">
-        <video src={props.src} width="100%" height="auto" autoPlay muted loop>
+        <video src={props.src} width="100%" height="auto" autoPlay muted loop playsInline>
         </video>
     </div>)
 }
