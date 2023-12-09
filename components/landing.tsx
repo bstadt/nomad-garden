9d6cc3e23cb907e490a5e5b30385481b72f4e80c
bstadt <brandonduderstadt@gmail.com>
2023-12-09 17:35:51 +0100

    a few smol updates

--- components/landing.tsx
+++ components/landing.tsx
@@ -66,8 +66,8 @@ export default function Landing({ slugs, metas }) {
                 <div className="w-full md:w-1/2 p-4 flex justify-center h-screen">
                     <div>
                         <div className="text-center pb-8">
-                            <p>Hi, I'm Brandon. This is my corner of the internet.</p>
-                            <p>I spend most of my time working as the founder and CEO of <a href={'https://home.nomic.ai/'} className={'cursor-pointer hover:underline'}>Nomic</a>.</p>
+                            <p>Hi, I'm <a href={'/posts/bio'} className={'cursor-pointer underline'}>Brandon</a>. This is my corner of the internet.</p>
+                            <p>I spend most of my time working as the founder and CEO of <a href={'https://nomic.ai/'} className={'cursor-pointer underline'}>Nomic</a>.</p>
                             <p>I spend the rest of my time doing things without considering their utility.</p>
                             <p>I've written about some of these things here:</p>
                         </div>
